ea9bb082fc0b7583cdeb52279ebd1a24e7c491c7
PrudhviRaj <pnookala@deloitte.com>
2018-09-02 03:52:04 +0530

    working

--- app.js
+++ app.js
@@ -1,49 +1,6 @@
-// var express = require("express");
-// var app = express();
-// var port = 3000;
-
-// var mongoose = require("mongoose");
-// var bodyParser = require('body-parser');
-
-
-
-// var nameSchema = new mongoose.Schema({
-//     firstName: String,
-//     lastNameName: String
-// });
-
-// var User = mongoose.model("User", nameSchema);
-
-// mongoose.Promise = global.Promise;
-// mongoose.connect("mongodb://localhost:27017/node-server");
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
-
-// app.listen(port, () => {
-//     console.log("Server listening on port " + port);
-// });
-
-// app.get("/", (req, res) => {
-//     res.sendFile(__dirname + "/index.html");
-// });
-
-// app.post("/addname", (req, res) => {
-//     var myData = new User(req.body);
-//     myData.save()
-//         .then(item => {
-//             res.send("item saved to database");
-//         })
-//         .catch(err => {
-//             res.status(400).send("unable to save to database");
-//         });
-// });
-
-var express=require('express');
+var express = require('express');
 var nodemailer = require("nodemailer");
-var app=express();
+var app = express();
 /*
     Here we are configuring our SMTP Server details.
     STMP is mail server which is responsible for sending and recieving email.
@@ -60,29 +17,29 @@ var smtpTransport = nodemailer.createTransport({
 
 /*------------------Routing Started ------------------------*/
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
-app.get('/send',function(req,res){
-    var mailOptions={
-        to : req.query.to,
-        subject : req.query.subject,
-        text : req.query.text
+app.get('/send', function (req, res) {
+    var mailOptions = {
+        to: req.query.to,
+        subject: req.query.subject,
+        html: req.query.text
     }
     console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function(error, response){
-     if(error){
+    smtpTransport.sendMail(mailOptions, function (error, response) {
+        if (error) {
             console.log(error);
-        res.end("error");
-     }else{
+            res.end("error");
+        } else {
             console.log("Message sent: " + response.message);
-        res.end("sent");
-         }
-});
+            res.end("sent");
+        }
+    });
 });
 
 /*--------------------Routing Over----------------------------*/
 
-app.listen(process.env.PORT || 3000,function(){
+app.listen(process.env.PORT || 3000, function () {
     console.log("Express Started on Port 3000");
 });
